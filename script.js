d90aca61e7a3147ba8ef31e9cd9ec411f05ba244
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-24 05:09:08 -0800

    Add backspace capability

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let numB = ``;
 let lastOperator = ``;
 let operator = ``;
 let display = document.querySelector(`.display`);
-let btnNumber = document.querySelectorAll(`.number`);
 
 reset();
 
+let btnNumber = document.querySelectorAll(`.number`);
 btnNumber.forEach(btn => {
   btn.addEventListener(`click`, (e) => {
     if (operator === ``){
@@ -30,7 +30,9 @@ btnOperator.forEach(btn => {
       numB = ``;
       display.textContent = numA;
     }
-    lastOperator = operator;
+    if (operator !== `=`){
+      lastOperator = operator;
+    }
   })
 }
 );
@@ -38,6 +40,28 @@ btnOperator.forEach(btn => {
 let btnClear = document.querySelector(`.clear`);
 btnClear.addEventListener(`click`, (e) => {
   reset();
+})
+
+let btnBackspace = document.querySelector(`.backspace`);
+btnBackspace.addEventListener(`click`, (e) => {
+  if (operator === ``){
+    if (numA.length > 1) {
+      numA = numA.substring(0, numA.length - 1);
+    }
+    else {
+      numA = `0`;
+    }
+    display.textContent = numA;
+  }
+  else { 
+    if (numB.length > 1){
+      numB = numB.substring(0, numB.length - 1);
+    }
+    else {
+      numB = `0`;
+    }
+    display.textContent = numB;
+  }
 })
 
 function reset(){
@@ -48,19 +72,19 @@ function reset(){
   display.textContent = 0;
 }
 
-function add(a, b){
+function add(a, b = 0){
   return a + b;
 }
 
-function subtract(a, b){
+function subtract(a, b = 0){
   return a - b;
 }
 
-function multiply(a, b){
+function multiply(a, b = 1){
   return a * b;
 }
 
-function divide(a, b){
+function divide(a, b = 1){
   return a / b;
 }
 
